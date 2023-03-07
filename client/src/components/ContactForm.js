463c1514b245f847888cd179ded4db23aea450a0
abbasi512 <tanzeelabbaskhan7@gmail.com>
2023-03-07 06:48:11 -0500

    test 2

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -5,6 +5,7 @@ function ContactForm() {
     <div className="form-container">
       <h1>Send a message to us!</h1>
       <form>
+        {/*  */}
         <input placeholder="Name" />
         <input placeholder="Email" />
         <input placeholder="Subject" />
