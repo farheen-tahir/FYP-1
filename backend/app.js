ab0a0063c2ea8c4629e45c7ee134b8e311060e9b
farheen-tahir <102976931+farheen-tahir@users.noreply.github.com>
2023-03-16 15:34:00 +0500

    Add files via upload
    backend-authentication-done

--- backend/app.js
+++ backend/app.js
@@ -1,73 +1,39 @@
+require("dotenv").config();
 const express=require("express");
 const mongoose=require("mongoose");
-const bodyParser=require("body-parser");
 const cors=require("cors");
-const bcrypt=require("bcrypt");
-const saltRounds=10;
+const fileUpload=require("express-fileupload");
+const cookieParser=require("cookie-parser");
 
 
 const app=express();
 app.use(express.json());
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
+app.use(cookieParser());
+app.use(fileUpload({
+    useTempFiles:true
+}));
 
-mongoose.connect("mongodb://localhost:27017/EAid");
-const userSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String,
-    type:String
-});
-const USER=mongoose.model("User",userSchema);
-app.get("/",function(req,res) {
+//ROUTES
+app.use("/user",require("./routes/userRouter"));
+app.use("/subscriber",require("./routes/subscriberRouter"));
+app.use("/api",require("./routes/upload"));
 
-});
-app.post("/signin",function(req,res) {
-    const {email,password}=req.body;
-    USER.findOne({email:email},(err,user)=>{
-        if(user) {
-            bcrypt.compare(password, user.password, function(err, result) {
-                if(result===true) {
-                    res.send({message:"success ",user:user});
-                    console.log("success");
-                } else {
-                    res.send({message:"not same password"});
-                    console.log("pin galat");
-                }    
-            });
-           
-        }
-        else {
-            res.send({message:"no user exist in the system"});
-            console.log("no availablle");
-        }
-    });
-});
-app.post("/signup",function(req,res){
-    const {name,email,password,type}=req.body;
-    bcrypt.hash(password, saltRounds).then(function(hash) {
-        USER.findOne({email:email},(err,user)=>{
-            if(user) {
-                res.send({message:"already exist"});
-                
-            } else {
-                const user=new USER ({
-                    name:name,email:email,password: hash,type:type
-                });
-                user.save(function(err) {
-                    if(err) {
-                        res.send(err);
-                    }else {
-                        res.send({message:"success"});
-                    }
-                });
-            }
-        });   
-    });
-    // console.log(req.body);
-    
-});
-app.listen(5000,function(){
-    console.log("server is running on port 5000");
+//DB CONNECTIVITY
+mongoose.connect(process.env.MONGODB_URL);
+// ,{
+//     useCreateIndex:true,
+//     useFindAndModify:false,
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// },err=>{
+//     if(err) throw err;
+//     console.log("Connected to DataBase");
+// }
+
+
+const PORT=process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log("SERVER IS RUNNING ON PORT NO. ",PORT)
 });
