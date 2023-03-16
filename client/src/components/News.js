a6a818f6a9f0ef19dfce71c2cd5b317b42360a1d
abbasi512 <tanzeelabbaskhan7@gmail.com>
2023-03-16 05:52:13 -0400

    deleted proxy

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -4,14 +4,13 @@ import './Card.css'
 
 const News = () => {
 
-
     const API_KEY = '09ccb245d16b4836af0f2802cdaa1b0b';
 
     const [news, setNews] = useState([])
     const [loading, setLoading] = useState(true);
 
 const fetchNews = async () =>{
-    let data = await fetch(`https://newsapi.org/v2/everything?q=Pakistan AND disaster&apiKey=${API_KEY}`)
+    let data = await fetch(`https://newsapi.org/v2/everything?q= Pakistan AND disaster &apiKey=${API_KEY}`)
     let parsedData = await data.json();
     setNews(parsedData.articles)
 
